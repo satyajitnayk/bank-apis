a38302ef31f32c670f706b3cea4f2227423eaea4
satyajit nayak <satyajitn15@gmail.com>
2024-01-13 13:14:17 +0530

    add test for entry.sql.go

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func createRandomAccount(t *testing.T) Account {
+func CreateRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -32,11 +32,11 @@ func createRandomAccount(t *testing.T) Account {
 }
 
 func TestCreateAccount(t *testing.T) {
-	createRandomAccount(t)
+	CreateRandomAccount(t)
 }
 
 func TestGetAccount(t *testing.T) {
-	account1 := createRandomAccount(t)
+	account1 := CreateRandomAccount(t)
 	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
 
 	require.NoError(t, err)
@@ -51,7 +51,7 @@ func TestGetAccount(t *testing.T) {
 }
 
 func TestUpdateAccount(t *testing.T) {
-	account1 := createRandomAccount(t)
+	account1 := CreateRandomAccount(t)
 
 	arg := UpdateAccountParams{
 		ID:      account1.ID,
@@ -71,7 +71,7 @@ func TestUpdateAccount(t *testing.T) {
 }
 
 func TestDeleteAccount(t *testing.T) {
-	account1 := createRandomAccount(t)
+	account1 := CreateRandomAccount(t)
 
 	err := testQueries.DeleteAccount(context.Background(), account1.ID)
 
@@ -87,7 +87,7 @@ func TestDeleteAccount(t *testing.T) {
 
 func TestListAccounts(t *testing.T) {
 	for i := 0; i < 10; i++ {
-		createRandomAccount(t)
+		CreateRandomAccount(t)
 	}
 
 	arg := ListAccountsParams{
---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,97 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/satyajitnayk/bank-apis/util"
+	"github.com/stretchr/testify/require"
+)
+
+// The *testing.T type provides methods and utilities for reporting test
+// failures and logging messages during the execution of your tests.
+func CreateRandomEntry(t *testing.T, accountId int64) Entry {
+	arg := CreateEntryParams{
+		AccountID: accountId,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.AccountID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	account := CreateRandomAccount(t)
+	CreateRandomEntry(t, account.ID)
+}
+
+func TestCreateEntry_Errors(t *testing.T) {
+	// Test case for a database query error
+	_, err := testQueries.CreateEntry(context.Background(), CreateEntryParams{})
+	require.Error(t, err)
+
+	// Test case for a SQL syntax error
+	_, err = testQueries.CreateEntry(context.Background(), CreateEntryParams{AccountID: 1, Amount: 100})
+	require.Error(t, err)
+}
+
+func TestGetEntry(t *testing.T) {
+	account := CreateRandomAccount(t)
+	entry1 := CreateRandomEntry(t, account.ID)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestGetEntry_Errors(t *testing.T) {
+	// Test case for a database query error
+	_, err := testQueries.GetEntry(context.Background(), 0)
+	require.Error(t, err)
+	// Test case for a non-existent entry (simulating sql.ErrNoRows)
+	nonExistentID := int64(999999)
+	_, err = testQueries.GetEntry(context.Background(), nonExistentID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+}
+
+func TestListEntries(t *testing.T) {
+	account := CreateRandomAccount(t)
+
+	for i := 0; i < 10; i++ {
+		CreateRandomEntry(t, account.ID)
+	}
+
+	arg := ListEntriesParams{
+		AccountID: account.ID,
+		Limit:     5,
+		Offset:    5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
